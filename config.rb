808853893129ecf41cb863c3e39a406d2f6aab1d
Kevin Manssat <kevin.manssat@hotmail.fr>
2017-12-18 10:35:31 +0100

    Base

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'assets/css'
+sass_dir = 'assets/css'
+images_dir = 'assets/img'
+javascripts_dir = 'assets/js'
+relative_assets = true
+line_comments = false
+output_style = :neasted
